ff759d51a957466c98aed39bc7f5a13b6ba453e3
matheus <matheus2060@gmail.com>
2020-11-29 13:47:57 -0300

    genric page color correction added and access page bug fix

--- acessar.js
+++ acessar.js
@@ -1,7 +1,7 @@
 //consertando a lingua da pagina
 document.getElementsByTagName("html")[0].lang = "pt-br";
 
-  var a = document.getElementsByTagName ("link")[0];
-  a.remove();
+var a = document.getElementsByTagName ("link")[0];
+a.remove();
 
-  document.getElementsByClassName("logo")[0].setAttribute("href","");
+document.getElementsByClassName("logo")[0].setAttribute("href","");
