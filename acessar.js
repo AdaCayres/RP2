fd588ac8aaf0f8e069f99e9ae7aeaf48704a5471
matheus <matheus2060@gmail.com>
2020-12-04 11:01:41 -0300

    access page corrections

--- acessar.js
+++ acessar.js
@@ -4,4 +4,11 @@ document.getElementsByTagName("html")[0].lang = "pt-br";
 var a = document.getElementsByTagName ("link")[0];
 a.remove();
 
-document.getElementsByClassName("logo")[0].setAttribute("href","");
+var logo = document.getElementsByClassName("logo")[0];
+logo.setAttribute("href","https://edisciplinas.usp.br/acessar/");
+logo.setAttribute("alt", "link para página de acesso");
+//logo.innerHTML = '<div class="inner-logo">https://edisciplinas.usp.br/acessar/</div>';
+logo.remove();
+
+var navigation = document.getElementsByClassName("navigation-menu")[0];
+navigation.innerHTML = "<li class='active'><a href='#welcome-section'>Bem-vindo</a></li>" + navigation.innerHTML;
