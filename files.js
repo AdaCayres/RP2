df2ae8a5d41f62c0d3ca913639e7e9883fdc53d5
joprcamargo <58490789+joprcamargo@users.noreply.github.com>
2020-12-09 14:38:08 -0300

    Update files.js

--- files.js
+++ files.js
@@ -3,3 +3,17 @@ var a_tags = document.getElementsByTagName("a");
 for (var i = 0; i < a_tags.length; i++) {
   a_tags[i].removeAttribute("title");
 }
+
+element = document.getElementsByClassName("fp-value")[1];
+element.parentNode.removeChild(element);
+
+//Elementos fieldset sem descrição
+var fieldset_coursesearchbox = document.getElementsByClassName(
+  "coursesearchbox invisiblefieldset"
+);
+
+fieldset_coursesearchbox[0].innerHTML =
+  "<legend>Buscar disciplinas</legend>" + fieldset_coursesearchbox[0].innerHTML;
+fieldset_coursesearchbox[1].innerHTML =
+  "<legend>Acção buscar disciplinas</legend>" +
+  fieldset_coursesearchbox[1].innerHTML;
