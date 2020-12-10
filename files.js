232b586b1ed76261c56054bdfeefc4600835d9ac
joprcamargo <jprcamargo@usp.br>
2020-12-10 10:18:38 -0300

    Update files.js

--- files.js
+++ files.js
@@ -5,59 +5,28 @@ for (var i = 0; i < a_tags.length; i++) {
 }
 
 window.onload = setTimeout(function () {
-  //debugger;
-  console.log(document);
+  var out_tag = document.getElementsByClassName(
+    "col-md-9 form-inline felement"
+  )[0];
+  var new_tag = document.getElementsByTagName("fieldset")[0];
+
+  var out_tag2 = document.getElementsByClassName(
+    "col-md-9 form-inline felement"
+  )[1];
+  var new_tag2 = document.getElementsByTagName("fieldset")[1];
+
+  var out_tag3 = document.getElementsByClassName("mb-3")[2];
+  var new_tag3 = document.getElementsByTagName("fieldset")[2];
+
+  out_tag.innerHTML = new_tag.innerHTML;
+  out_tag2.innerHTML = new_tag2.innerHTML;
+  out_tag3.innerHTML = new_tag3.innerHTML;
+
   var element = document.querySelector("ul.fp-value");
-  console.log("salve ", element);
   element.remove();
-
-  const form = document.createElement("form");
-  form.id = "form-fieldset";
-  const parent = document.getElementsByClassName("mb-3")[2];
-  while (parent.childNodes.length) {
-    form.appendChild(parent.firstChild);
-  }
-
-  const form_1 = document.createElement("form");
-  form_1.id = "form-fieldset1";
-  const form_2 = document.createElement("form");
-  form_2.id = "form-fieldset2";
-  var forms = [form_1, form_2];
-  const parent2 = document.querySelectorAll(".col-md-9.form-inline.felement");
-  for (var i = 0; i < parent2.length; i++) {
-    while (parent2[i].childNodes.length) {
-      forms[i].appendChild(parent2[i].firstChild);
-    }
-  }
-  parent.appendChild(form);
-  parent2[0].appendChild(form_1);
-  parent2[1].appendChild(form_2);
-
   const submit = document.createElement("input");
   submit.type = "submit";
   submit.className = "submit-form";
+  var form = document.querySelector(".mform.clearfix");
   form.appendChild(submit);
-
-  const submit2 = document.createElement("input");
-  submit2.type = "submit";
-  submit2.className = "submit-form";
-  var form2 = document.querySelector(".mform.clearfix");
-  form2.appendChild(submit2);
-
-  const submit3 = document.createElement("input");
-  submit3.type = "submit";
-  submit3.className = "submit-form";
-  var form3 = document.querySelector("#form-fieldset");
-  form3.appendChild(submit3);
-
-  const submit4 = document.createElement("input");
-  submit4.type = "submit";
-  submit4.className = "submit-form";
-  var form3 = document.querySelector("#form-fieldset1");
-  form3.appendChild(submit4);
-  const submit5 = document.createElement("input");
-  submit5.type = "submit";
-  submit5.className = "submit-form";
-  var form5 = document.querySelector("#form-fieldset2");
-  form3.appendChild(submit5);
 }, 5000);
